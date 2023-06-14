1b72f8bc86aefd266e276cdcc74fecfebd50340f
sangramsir <136458988+sangramsir@users.noreply.github.com>
2023-06-14 20:45:08 +0530

    Add files via upload

---
+++ Sangram-Assignment1.py
@@ -0,0 +1,23 @@
+print("My name is sangram")
+print("This is assignment-1")
+
+a=int(input("enter the marks1 "))
+
+b=int(input("enter the marks2 "))
+
+c=int(input("enter the marks3 "))
+
+print("marks1 =",a)
+print("marks2 =",b)
+print("marks3 =",c)
+avg=(a+b+c)/3
+print("Average of ",a,"",b,"",c," is",avg)
+#My name is sangram
+#This is assignment-1
+#enter the marks1 79 
+#enter the marks2 90
+#enter the marks3 87
+#marks1 = 79
+#marks2 = 90
+#marks3 = 87
+#Average of  79  90  87  is 85.33333333333333
